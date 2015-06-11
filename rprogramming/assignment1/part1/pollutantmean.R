763c4faec1aea30a352840ffe037c2ae552bb94d
Chris Beggio <beggio@gmail.com>
2015-06-11 06:30:39 -0600

    Added id string split logic and file processing loop.

--- rprogramming/assignment1/part1/pollutantmean.R
+++ rprogramming/assignment1/part1/pollutantmean.R
@@ -15,6 +15,20 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
 ## in the 'id' vector (ignoring NA values)
 ## NOTE: Do not round the result!
 
+# Open each file in the range of id, and read in the non-NA 
+# values of pollutant
+  
+print(directory,pollutant,id)  
+# Determine first and last filename
+  
+firstid <- strsplit(id,":")[[1]][1]
+lastid <- strsplit(id,":")[[1]][2]
 
+
+for (index in id) {
+  # Concatenate filename
+  filename <- paste(index,".csv")
+  df$index <- read.csv(directory/index)
+}  
 
 }
